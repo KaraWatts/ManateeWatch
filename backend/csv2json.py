d04dc25e10c06f4aec430fc69386f1b3f0d11b63
Kara Watts <kdwwatts@gmail.com>
2024-04-04 19:48:34 -0400

    reformatted to accept inaturalist photos

--- backend/csv2json.py
+++ backend/csv2json.py
@@ -21,17 +21,24 @@ def import_sightings(file_path):
         reader = csv.DictReader(file)
         for row in reader:
              # Parse and format date strings
-            sighting_date = datetime.strptime(row['SIGHTINGDATE'], '%Y-%m-%dT%H:%M:%S%z')
-            created_date = datetime.strptime(row['DATECREATED'], '%Y-%m-%dT%H:%M:%S%z')
+            try:
+                sighting_date = datetime.strptime(row['SIGHTINGDATE'], '%Y-%m-%dT%H:%M:%S%z')
+                created_date = datetime.strptime(row['DATECREATED'], '%Y-%m-%dT%H:%M:%S%z')
+            except:
+                sighting_date = datetime.strptime(row['SIGHTINGDATE'], '%Y-%m-%d %H:%M:%S %Z')
+                created_date = datetime.strptime(row['DATECREATED'], '%Y-%m-%d %H:%M:%S %Z')
 
+            # Convert "null" strings to None
+            num_adults = int(row['NUMBER_ADULT_MANATEES']) if row['NUMBER_ADULT_MANATEES'] != 'Null' else None
+            num_calf = int(row['NUMBER_CALF_MANATEES']) if row['NUMBER_CALF_MANATEES'] != 'Null' else None
 
 
             Sighting_Data.objects.create(
                 lat=row['lat'],
                 lon=row['lon'],
                 User_ID=row['User_ID'],
-                Num_Adults=row['NUMBER_ADULT_MANATEES'],
-                Num_Calf=row['NUMBER_CALF_MANATEES'],
+                Num_Adults=num_adults,
+                Num_Calf=num_calf,
                 Activity=row['ACTIVITY'],
                 Comments=row['COMMENTS'],
                 Sighting_date=sighting_date.strftime('%Y-%m-%d %H:%M:%S'),  # Format to Django's expected format
--- backend/map_app/models.py
+++ backend/map_app/models.py
@@ -5,11 +5,13 @@ class Sighting_Data(models.Model):
     lat = models.DecimalField(max_digits=25, decimal_places=10)
     lon = models.DecimalField(max_digits=25, decimal_places=10)
     User_ID = models.CharField()
-    Num_Adults = models.SmallIntegerField()
-    Num_Calf = models.SmallIntegerField()
-    Activity = models.CharField()
-    Comments = models.TextField(null=True)
+    Num_Adults = models.SmallIntegerField(null=True, blank=True)
+    Num_Calf = models.SmallIntegerField(null=True, blank=True)
+    Activity = models.CharField(null=True, blank=True)
+    Comments = models.TextField(null=True, blank=True)
     Sighting_date = models.DateTimeField()
     Created_date = models.DateTimeField()
-    Image = models.ImageField(null=True)
+    Image = models.CharField(null=True)
+    # Image = models.ImageField(null=True)
+
     
