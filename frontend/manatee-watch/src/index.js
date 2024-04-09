528baefa975732f2fc26bc79a6a31ef14d9c0587
Kara Watts <kdwwatts@gmail.com>
2024-04-09 09:35:22 -0400

    used navigator.mediaDevices to access camera and capture using Canvas - result is bad image quality

--- frontend/manatee-watch/src/index.js
+++ frontend/manatee-watch/src/index.js
@@ -8,9 +8,7 @@ import { RouterProvider } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
 );
 
 // If you want your app to work offline and load faster, you can change
