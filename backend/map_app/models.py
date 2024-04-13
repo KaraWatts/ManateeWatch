795bfd4324f8842d13d49fcc64926326e99e9a6f
Kara Watts <kdwwatts@gmail.com>
2024-04-12 21:15:54 -0400

    create profile model and connect to user and sightings

--- backend/map_app/models.py
+++ backend/map_app/models.py
@@ -1,11 +1,12 @@
 from django.db import models
-from user_app.models import Client
+from profile_app.models import User_Profile
+
 
 # Create your models here.
 class Sighting_Data(models.Model):
     lat = models.DecimalField(max_digits=40, decimal_places=15)
     lon = models.DecimalField(max_digits=40, decimal_places=15)
-    user = models.ForeignKey(Client, on_delete=models.SET_NULL, related_name="sightings", null=True)
+    user = models.ForeignKey(User_Profile, on_delete=models.SET_NULL, related_name="sightings", null=True)
     data_source = models.CharField(null=True)
     num_Adults = models.SmallIntegerField(default=0, null=True)
     num_Calf = models.SmallIntegerField(default=0, null=True)
---
+++ backend/reactions_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ReactionsAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'reactions_app'
