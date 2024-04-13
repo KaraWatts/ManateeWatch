09ec98bdb604efadb729f98138ff90aa7c612f86
Kara Watts <kdwwatts@gmail.com>
2024-04-13 00:37:24 -0400

    created profile views to view and edit user profile

--- backend/profile_app/urls.py
+++ backend/profile_app/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from .views import ViewAllProfiles, View_Profile, EditProfile
 
 urlpatterns = [
-    path('', ViewAllProfiles.as_view(), name="all_profiles"),
-    path('<int:user_id>/', View_Profile.as_view(), name="profile_by_id"),
-    path('<int:user_id>/edit/', EditProfile.as_view(), name='edit_profile')
-
+    path('', View_Profile.as_view(), name="profile_by_id"),
 ]
--- backend/profile_app/views.py
+++ backend/profile_app/views.py
@@ -1,13 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from user_app.views import TokenReq
 from rest_framework.views import APIView
+from .models import User_Profile
+from .serializers import ProfileSerializer
+from rest_framework.response import Response
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_406_NOT_ACCEPTABLE,
+    HTTP_201_CREATED,
+    HTTP_400_BAD_REQUEST
+)
+from manateewatch_proj.settings import env
+import requests
+from django.http import JsonResponse
 
 # Create your views here.
-class ViewAllProfiles(APIView):
-    pass
+class ViewProfile(TokenReq):
+    def get(self, request):
+        user = request.user.id
+        profile = get_object_or_404(User_Profile, id=user)
+        ser_profile = ProfileSerializer(profile)
+        return Response(ser_profile.data, status=HTTP_200_OK)
 
-class View_Profile(APIView):
-    pass
-
-class EditProfile(TokenReq):
-    pass
+    def put(self, request):
+        data = request.data.copy()
+        user = request.user.id
+        profile = get_object_or_404(User_Profile, id=user)
+        edit_profile = ProfileSerializer(profile, data=data, partial=True)
+        if edit_profile.is_valid():
+            edit_profile.save()
+            return Response(data, status=HTTP_201_CREATED)
+        print(edit_profile.errors)
+        return Response(edit_profile.errors, status=HTTP_400_BAD_REQUEST)
