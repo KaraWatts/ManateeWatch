1e4bfaf4fb3373f5dfec20717cf08600cc20636a
Kara Watts <kdwwatts@gmail.com>
2024-04-15 22:20:49 -0400

    edited csv to json converter to pull profile data for the data source profile

--- backend/csv2json.py
+++ backend/csv2json.py
@@ -13,30 +13,22 @@ django.setup()
 
 
 # Now import Django models
-from map_app.models import Sighting_Data  # Replace 'myapp' with your actual app name
-
+from map_app.models import Sighting_Data  
+from profile_app.models import User_Profile
 
 def import_sightings(file_path):
     with open(file_path, 'r') as file:
         reader = csv.DictReader(file)
         for row in reader:
-            #  # Parse and format date strings
-            # try:
-            #     sighting_date = datetime.strptime(row['SIGHTINGDATE'], '%Y-%m-%dT%H:%M:%S%z')
-            #     created_date = datetime.strptime(row['DATECREATED'], '%Y-%m-%dT%H:%M:%S%z')
-            # except:
-            #     sighting_date = datetime.strptime(row['SIGHTINGDATE'], '%Y-%m-%d %H:%M:%S %Z')
-            #     created_date = datetime.strptime(row['DATECREATED'], '%Y-%m-%d %H:%M:%S %Z')
 
-            # Convert "null" strings to None
             num_adults = int(row['NUMBER_ADULT_MANATEES']) if row['NUMBER_ADULT_MANATEES'] != 'Null' else None
             num_calf = int(row['NUMBER_CALF_MANATEES']) if row['NUMBER_CALF_MANATEES'] != 'Null' else None
-
+            user = User_Profile.objects.get(display_name="Data Source")
 
             Sighting_Data.objects.create(
                 lat=row['lat'],
                 lon=row['lon'],
-                user=None,
+                user=user,
                 data_source=row['data_source'],
                 num_Adults=num_adults,
                 num_Calf=num_calf,
