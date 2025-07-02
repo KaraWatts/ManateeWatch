69c2c68429820ebc7deaad9d22ebe65d17dd87c5
Kara Watts <kdwwatts@gmail.com>
2025-07-02 07:40:22 -0400

    made small change to push redeploy

--- backend/manateewatch_proj/urls.py
+++ backend/manateewatch_proj/urls.py
@@ -1,7 +1,7 @@
 """
 URL configuration for manateewatch_proj project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `url patterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
 Function views
